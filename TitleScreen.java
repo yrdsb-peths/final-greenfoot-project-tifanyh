0c941ec7f352cc4bba838bc85d173c9cef31b9c1
tifanyh <341118578@gapps.yrdsb.ca>
2024-01-11 13:30:40 -0500

    Title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -3,12 +3,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class TitleScreen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Tiffany Ho) 
+ * @version January 11 2024)
  */
 public class TitleScreen extends World
 {
-
+    Label titleLabel = new Label("Dinosaur Escape", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -16,6 +16,38 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 300, 1); 
+
+        addObject(titleLabel, 300, 100);
+        prepare();
+    }
+    
+    public void act()
+    {
+        //start the game if user presses the space bar.
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+
+        }
+    }
+    
+    
+    private void prepare()
+    {
+        Label label2 = new Label("Use \u2190 and \u2192 to Move", 20);
+        addObject(label2,300,230);
+
+        Label label = new Label("Press <space> to start the game", 20);
+        addObject(label,300,248);
+
+        Label label3 = new Label("Avoid the cactus and keep moving to win!", 20);
+        addObject(label3,300,268);
+
+        Dinosaur dinosaur = new Dinosaur();
+        addObject(dinosaur,300,180);
+        Floor floor = new Floor();
+        addObject(floor,299,35);
     }
 }
