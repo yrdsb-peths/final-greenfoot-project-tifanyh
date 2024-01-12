3828ee12a6367f50d52d6e403ecf8dc37b7737f9
tifanyh <341118578@gapps.yrdsb.ca>
2024-01-12 12:59:51 -0500

    title edit

--- TitleScreen.java
+++ TitleScreen.java
@@ -18,7 +18,7 @@ public class TitleScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 300, 1); 
 
-        addObject(titleLabel, 300, 100);
+        addObject(titleLabel, 300, 60);
         prepare();
     }
     
@@ -37,18 +37,18 @@ public class TitleScreen extends World
     private void prepare()
     {
         Label label2 = new Label("Use \u2190 and \u2192 to Move", 20);
-        addObject(label2,300,230);
+        addObject(label2,300,270);
 
         Label label = new Label("Press <space> to start the game", 20);
-        addObject(label,300,248);
+        addObject(label,300,120);
 
         Label label3 = new Label("Avoid the cactus and keep moving to win!", 20);
-        addObject(label3,300,268);
+        addObject(label3,300,100);
 
         Dinosaur dinosaur = new Dinosaur();
         addObject(dinosaur,300,170);
         
         Floor floor = new Floor();
-        addObject(floor,299,30);
+        addObject(floor,299,222);
     }
 }
