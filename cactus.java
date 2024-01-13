1f7bfa8d3bb2276a7a165f15cb91adffb84b7e17
tifanyh <341118578@gapps.yrdsb.ca>
2024-01-13 00:21:18 -0500

    editing cactus idle file

--- cactus.java
+++ cactus.java
@@ -12,8 +12,23 @@ public class cactus extends Actor
      * Act - do whatever the cactus wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootImage[] cacti = new GreenfootImage[7];
+    
     public void act()
     {
         // Add your action code here.
+        for(int i = 1; i < cacti.length; i++)
+        {
+            cacti[i] = new GreenfootImage("images/cactus_idle/caactus (" + i + ").png");
+            cacti[i].scale(100,70);
+        }
+        setImage(cacti[1]);
     }
+    
+    public void animateCacti()
+    {
+        
+    }
+    
+    
 }
