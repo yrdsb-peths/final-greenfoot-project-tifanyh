443c2ae948cb8213cc6b20194562e0c5c29b3ed6
tifanyh <341118578@gapps.yrdsb.ca>
2024-01-14 09:12:50 -0500

    cactus

--- cactus.java
+++ cactus.java
@@ -6,15 +6,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (Tiffany Ho) 
  * @version (January 11 2024)
  */
-public class cactus extends Actor
+public class Cactus extends Actor
 {
     /**
      * Act - do whatever the cactus wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     GreenfootImage[] cacti = new GreenfootImage[7];
+    int imageIndex = 0;
+    int animationCounter = 0;
+    int animationDelay = 3;
+    int moveSpeed = 2;
     
-    public void act()
+    public Cactus()
     {
         // Add your action code here.
         for(int i = 1; i < cacti.length; i++)
@@ -25,9 +29,26 @@ public class cactus extends Actor
         setImage(cacti[1]);
     }
     
-    public void animateCacti()
+    public void act()
     {
-        
+        animateCactus();
+        setLocation(getX() - moveSpeed, getY());
+
+        if(getX() < -getImage().getWidth()) 
+        {
+            setLocation(getWorld().getWidth(), Greenfoot.getRandomNumber(getWorld().getHeight()));
+        }
+    }
+    
+    public void animateCactus()
+    {
+        if(animationCounter == animationDelay) {
+            setImage(cacti[imageIndex]);
+            imageIndex = (imageIndex + 1) % cacti.length;
+            animationCounter = 0;
+        } else {
+            animationCounter++;
+        }
     }
     
     
